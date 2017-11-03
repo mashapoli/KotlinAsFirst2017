36eac4234cae3b86c7330c9dc9821f58f7e419ba
Мария <mashapolichuk911@gmail.com>
2017-11-04 00:08:09 +0300

    1 задача из 5 урока

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -38,6 +38,7 @@ class Tests {
         assertEquals("18.11.2018", dateStrToDigit("18 ноября 2018"))
         assertEquals("", dateStrToDigit("23"))
         assertEquals("03.04.2011", dateStrToDigit("3 апреля 2011"))
+        assertEquals("03.04.1", dateStrToDigit("3 апреля 1"))
     }
 
     @Test
